28bcd64173730c2cb3115086426922182c8d4df7
garryshtopor <garryshtopor@gmail.com>
2025-06-02 18:01:20 +0500

    step 1 completed

--- build/lib/gendiff/scripts/gendiff.py
+++ build/lib/gendiff/scripts/gendiff.py
@@ -8,8 +8,3 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    main()

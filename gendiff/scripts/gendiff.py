2025d9f2ad9f5f62206424826b1a30a88f7b77dd
garryshtopor <garryshtopor@gmail.com>
2025-06-02 19:13:26 +0500

    Step 3 completed

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,27 @@
 import argparse
+import json
+
+
+def generate_diff(first_file, second_file):
+    with open(first_file) as f1, open(second_file) as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+    
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+    diff = []
+    
+    for key in all_keys:
+        if key not in data1:
+            diff.append(f'  + {key}: {str(data2[key]).lower()}')
+        elif key not in data2:
+            diff.append(f'  - {key}: {str(data1[key]).lower()}')
+        elif data1[key] == data2[key]:
+            diff.append(f'    {key}: {str(data1[key]).lower()}')
+        else:
+            diff.append(f'  - {key}: {str(data1[key]).lower()}')
+            diff.append(f'  + {key}: {str(data2[key]).lower()}')
+    
+    return '{\n' + '\n'.join(diff) + '\n}'
 
 
 def main():
@@ -14,6 +37,8 @@ def main():
         default='stylish'
     )
     args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
     return args
 
 

a45b3d9e6a282ef53af0f67f439308e14c0f10d4
garryshtopor <garryshtopor@gmail.com>
2025-06-02 19:05:41 +0500

    step 2 completed

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,12 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        help='set format of output',
+        default='stylish'
+    )
     args = parser.parse_args()
     return args
 

7ec890ee57a6f62bfa2783eccfeae9aef49904a2
garryshtopor <garryshtopor@gmail.com>
2025-06-18 20:39:03 +0500

    fix python ci errors occured 3

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,12 +4,12 @@ from gendiff import generate_diff
 def test_generate_diff_json():
     file1_path = 'tests/fixtures/file1.json'
     file2_path = 'tests/fixtures/file2.json'
-    expected = '''
+    expected = '''{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-'''
+}'''
     assert generate_diff(file1_path, file2_path) == expected
